08a172da444ec6dc399327a86d23605ff032b253
lukehoward4 <lwhby7@umsystem.edu>
2024-10-09 16:47:12 -0500

    Added comments to State.py

--- State.py
+++ State.py
@@ -2,17 +2,22 @@ class State():
     def __init__(self, playerSquares, oppSquares):
         self.__playerSquares = playerSquares
         self.__oppSquares = oppSquares
-
+    #returns the coordinates of all squares currently held by the player in the form of a list of two element lists
     def getPlayerSquares(self):
         return self.__playerSquares
+    #Appends the list of opponent squares based on passed coordinate
+    #If the coordinate is already held by one player, False is returned. Else, True.
     def addPlayerSquare(self, coordinate):
         if((coordinate not in self.__playerSquares) and (coordinate not in self.__oppSquares)):
             self.__playerSquares.append(coordinate)
             return True
         else:
             return False
+    #returns the coordinates of all squares currently held by the opponent in the form of a list of two element lists
     def getOppSquares(self):
         return self.__oppSquares
+    #Appends the list of opponent squares based on passed coordinate
+    #If the coordinate is already held by one player, False is returned. Else, True.
     def addOppSquare(self, coordinate):
         if((coordinate not in self.__playerSquares) and (coordinate not in self.__oppSquares)):
             self.__oppSquares.append(coordinate)
